fc14a78a81d47d77cce347de6bc9e73349a04c3a
camillacrane <102168879+camillacrane@users.noreply.github.com>
2023-08-11 16:52:10 -0400

    mobile nav bar done

--- components/DropdownMenu.tsx
+++ components/DropdownMenu.tsx
@@ -9,18 +9,18 @@ const DropdownMenu = () =>  {
     return(
         <div className="">
             <Menu as="div" className="relative">
-                <Menu.Button className="inline-flex justify-center w-full   text-black">
+                <Menu.Button className="inline-flex justify-center w-full   text-white md:text-black">
                     Listen <IoIosArrowDown size={18} className="ml-1 mt-1"/>
                 </Menu.Button>
-                <Menu.Items className="origin-top absolute left-1/2 transform -translate-x-1/2 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                    <div className="flex justify-center items-center text-medium hover:bg-gray-200 hover:rounded-md text-black w-full px-4 py-2">
+                <Menu.Items className="origin-top absolute left-1/2 transform -translate-x-1/2 mt-2 w-48 rounded-sm shadow-lg bg-neutral-800 md:bg-white ring-1 ring-black ring-opacity-5">
+                    <div className="flex justify-center items-center text-medium md:hover:bg-gray-200 hover:rounded-md text-white md:text-black w-full px-4 py-2">
                         <Menu.Item>
                             <a href="https://apps.apple.com/us/app/wxyc-radio/id353182815" target="_blank">
                                 iPhone app
                             </a>
                         </Menu.Item>
                     </div>
-                    <div className="flex justify-center items-center text-medium hover:bg-gray-200 rounded-md text-black w-full px-4 py-2">
+                    <div className="flex justify-center items-center text-medium md:hover:bg-gray-200 rounded-md text-white md:text-black  w-full px-4 py-2">
                         <Menu.Item>
                             <a href="/playlist">
                                 Live playlist
