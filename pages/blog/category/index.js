d99930f4bfb7a328e1f2b876c3441d47229cf521
Hayley <haowens@email.unc.edu>
2023-08-15 14:44:31 -0400

    debugging blog page build

---
+++ pages/blog/category/index.js
@@ -0,0 +1,16 @@
+const CategoryPage = (props) => {
+  
+  return (
+    <div>
+        <p>category page</p>
+    </div>    
+      
+  );
+}
+
+export default CategoryPage;
+
+
+export const getStaticProps = async () => {
+  return {}
+}
